f3c00841e546fc03dcea6249a1c6c1544f3bfe38
John Griffiths <gnuchu@gmail.com>
2023-05-10 12:17:03 +0100

    commit sizing

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="20230510_113142_0000.png" width="1200px" alt="Mums Funeral Details" />
-      </header>
+        <img src="20230510_113142_0000.png" width="600px" alt="Mums Funeral Details" />
     </div>
   );
 }
